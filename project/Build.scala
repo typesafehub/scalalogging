0d63edfde8cc3e6b701292cfdedcddb588291d5d
Heiko Seeberger <heiko.seeberger@googlemail.com>
2012-04-23 20:13:33 +0200

    Configured build for Scala 2.10.0-SNAPSHOT.

--- project/Build.scala
+++ project/Build.scala
@@ -8,36 +8,36 @@ object Build extends Build {
     "scalalogging",
     file("."),
     settings = commonSettings ++ Seq(
-      libraryDependencies ++= Seq(
+/*      libraryDependencies ++= Seq(
         Dependencies.config,
         Dependencies.scalaz
-      )
+      )*/
     )
   )
 
   def commonSettings = 
     Defaults.defaultSettings ++ 
     Seq(
+      resolvers += Opts.resolver.sonatypeSnapshots,
       organization := "name.heikoseeberger.scalalogging",
-      scalacOptions ++= Seq("-unchecked", "-deprecation"),
-      libraryDependencies ++= Seq(
+      scalaVersion := "2.10.0-SNAPSHOT",
+      scalacOptions ++= Seq("-unchecked", "-deprecation", "-optimize"),
+/*      libraryDependencies ++= Seq(
         Dependencies.specs2,
         Dependencies.scalacheck,
         Dependencies.mockito,
         Dependencies.hamcrest
-      ),
+      ),*/
       initialCommands in console := "import name.heikoseeberger.scalalogging._"
     ) ++
     scalariformSettings
 
   object Dependencies {
     // Compile
-    val config = "com.typesafe" % "config" % "0.4.0"
-    val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.4"
     // Test
-    val specs2 = "org.specs2" %% "specs2" % "1.9" % "test"
-    val scalacheck = "org.scalacheck" %% "scalacheck" % "1.9" % "test"
-    val mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"
-    val hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1" % "test"
+//    val specs2 = "org.specs2" %% "specs2" % "1.9" % "test"
+//    val scalacheck = "org.scalacheck" %% "scalacheck" % "1.9" % "test"
+//    val mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"
+//    val hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1" % "test"
   }
 }
