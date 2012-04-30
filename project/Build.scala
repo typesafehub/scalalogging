56d35b7a8167e068c47c3e34aafca60969f77d37
Heiko Seeberger <heiko.seeberger@googlemail.com>
2012-04-30 17:14:48 +0200

    closes #7: Split tests into separate project because of macros

--- project/Build.scala
+++ project/Build.scala
@@ -5,45 +5,56 @@ import com.typesafe.sbtscalariform.ScalariformPlugin._
 object Build extends Build {
 
   lazy val root = Project(
-    "scalalogging",
+    "root",
     file("."),
     settings = commonSettings ++ Seq(
-/*      libraryDependencies ++= Seq(
-        Dependencies.config,
-        Dependencies.scalaz
-      )*/
+      publishArtifact := false
+    ),
+    aggregate = Seq(scalalogging, scalaloggingTest)
+  )
+
+  lazy val scalalogging = Project(
+    "scalalogging",
+    file("scalalogging"),
+    settings = commonSettings ++ Seq(
     )
   )
 
-  def commonSettings = 
-    Defaults.defaultSettings ++ Seq(
-      resolvers += Opts.resolver.sonatypeSnapshots,
-      organization := "name.heikoseeberger",
-      scalaVersion := "2.10.0-SNAPSHOT",
-      scalacOptions ++= Seq("-unchecked", "-deprecation", "-optimize"),
-      libraryDependencies ++= Seq(
+  lazy val scalaloggingTest = Project(
+    "scalalogging-test",
+    file("scalalogging-test"),
+    settings = commonSettings ++ Seq(
+    ),
+    dependencies = Seq(scalalogging)
+  )
+
+  def commonSettings = Defaults.defaultSettings ++ Seq(
+//    resolvers += Opts.resolver.sonatypeSnapshots,
+    organization := "name.heikoseeberger",
+    scalaVersion := "2.10.0-M3",
+    scalacOptions ++= Seq("-unchecked", "-deprecation", "-optimize"),
+    libraryDependencies ++= Seq(
 //        Dependencies.Test.Specs2
-      ),
-      initialCommands in console := "import name.heikoseeberger.scalalogging._",
-      licenses := Seq("Apache 2.0 License" -> url("http://www.apache.org/licenses/LICENSE-2.0.html")),
-      homepage := Some(url("https://github.com/hseeberger/scalalogging")),
-      publishTo <<= version(v => Option(if (v.trim endsWith "SNAPSHOT") Opts.resolver.sonatypeSnapshots else Opts.resolver.sonatypeStaging)),
-      publishArtifact in Test := false,
-      pomIncludeRepository := (_ => false),
-      pomExtra :=
-        <scm>
-          <url>https://github.com/hseeberger/scalalogging</url>
-          <connection>scm:git:git://github.com/hseeberger/scalalogging.git</connection>
-        </scm>
-        <developers>
-          <developer>
-            <id>hseeberger</id>
-            <name>Heiko Seeberger</name>
-            <url>http://heikoseeberger.name</url>
-          </developer>
-        </developers>
-    ) /*++
-    scalariformSettings*/
+    ),
+    initialCommands in console := "import name.heikoseeberger.scalalogging._",
+    licenses := Seq("Apache 2.0 License" -> url("http://www.apache.org/licenses/LICENSE-2.0.html")),
+    homepage := Some(url("https://github.com/hseeberger/scalalogging")),
+    publishTo <<= isSnapshot(isSnapshot => Some(if (isSnapshot) Opts.resolver.sonatypeSnapshots else Opts.resolver.sonatypeStaging)),
+    publishArtifact in Test := false,
+    pomIncludeRepository := (_ => false),
+    pomExtra :=
+      <scm>
+        <url>https://github.com/hseeberger/scalalogging</url>
+        <connection>scm:git:git://github.com/hseeberger/scalalogging.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>hseeberger</id>
+          <name>Heiko Seeberger</name>
+          <url>http://heikoseeberger.name</url>
+        </developer>
+      </developers>
+  )
 
   object Dependencies {
 
@@ -51,7 +62,7 @@ object Build extends Build {
     }
 
     object Test {
-      val Specs2 = "org.specs2" %% "specs2" % "1.9-SNAPSHOT" % "test"
+      val Specs2 = "org.specs2" %% "specs2" % "1.10-SNAPSHOT" % "test"
 //      val ScalaCheck = "org.scalacheck" %% "scalacheck" % "1.9" % "test"
 //      val Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"
 //      val Hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1" % "test"
--- scalalogging/src/main/scala/name/heikoseeberger/scalalogging/package.scala
+++ scalalogging/src/main/scala/name/heikoseeberger/scalalogging/package.scala
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
+/*
 package name.heikoseeberger
 
-/*
 package object scalalogging {
 }
 */
