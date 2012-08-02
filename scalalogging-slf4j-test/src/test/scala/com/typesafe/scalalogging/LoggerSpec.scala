f3474ee55ab745eb43f9a1ca78d44a48d8085489
Heiko Seeberger <heiko.seeberger@googlemail.com>
2012-08-02 23:07:58 +0200

    closes #11: Add parameterized log messages

--- scalalogging-slf4j-test/src/test/scala/com/typesafe/scalalogging/LoggerSpec.scala
+++ scalalogging-slf4j-test/src/test/scala/com/typesafe/scalalogging/LoggerSpec.scala
@@ -31,16 +31,92 @@ object LoggerSpec extends Specification with Mockito {
       val logger = Logger(underlying)
       logger.trace(Message)
       there was no(underlying).trace(Message)
+      logger.trace(Message, "p1")
+      there was no(underlying).trace(Message, "p1")
+      logger.trace(Message, "p1", "p2")
+      there was no(underlying).trace(Message, "p1", "p2")
+      logger.trace(Message, Array("p1", "p2", "p3"))
+      there was no(underlying).trace(Message, Array("p1", "p2", "p3"))
     }
   }
 
-  "Calling an enabled logging method" should {
+  "Calling the enabled error method" should {
     "result in the underlying method being called" in {
       val underlying = mock[Underlying]
       underlying.isErrorEnabled returns true
       val logger = Logger(underlying)
       logger.error(Message)
       there was one(underlying).error(Message)
+      logger.error(Message, "p1")
+      there was one(underlying).error(Message, "p1")
+      logger.error(Message, "p1", "p2")
+      there was one(underlying).error(Message, "p1", "p2")
+      logger.error(Message, Array("p1", "p2", "p3"))
+      there was one(underlying).error(Message, Array("p1", "p2", "p3"))
+    }
+  }
+
+  "Calling the enabled warn method" should {
+    "result in the underlying method being called" in {
+      val underlying = mock[Underlying]
+      underlying.isWarnEnabled returns true
+      val logger = Logger(underlying)
+      logger.warn(Message)
+      there was one(underlying).warn(Message)
+      logger.warn(Message, "p1")
+      there was one(underlying).warn(Message, "p1")
+      logger.warn(Message, "p1", "p2")
+      there was one(underlying).warn(Message, "p1", "p2")
+      logger.warn(Message, Array("p1", "p2", "p3"))
+      there was one(underlying).warn(Message, Array("p1", "p2", "p3"))
+    }
+  }
+
+  "Calling the enabled info method" should {
+    "result in the underlying method being called" in {
+      val underlying = mock[Underlying]
+      underlying.isInfoEnabled returns true
+      val logger = Logger(underlying)
+      logger.info(Message)
+      there was one(underlying).info(Message)
+      logger.info(Message, "p1")
+      there was one(underlying).info(Message, "p1")
+      logger.info(Message, "p1", "p2")
+      there was one(underlying).info(Message, "p1", "p2")
+      logger.info(Message, Array("p1", "p2", "p3"))
+      there was one(underlying).info(Message, Array("p1", "p2", "p3"))
+    }
+  }
+
+  "Calling the enabled debug method" should {
+    "result in the underlying method being called" in {
+      val underlying = mock[Underlying]
+      underlying.isDebugEnabled returns true
+      val logger = Logger(underlying)
+      logger.debug(Message)
+      there was one(underlying).debug(Message)
+      logger.debug(Message, "p1")
+      there was one(underlying).debug(Message, "p1")
+      logger.debug(Message, "p1", "p2")
+      there was one(underlying).debug(Message, "p1", "p2")
+      logger.debug(Message, Array("p1", "p2", "p3"))
+      there was one(underlying).debug(Message, Array("p1", "p2", "p3"))
+    }
+  }
+
+  "Calling the enabled trace method" should {
+    "result in the underlying method being called" in {
+      val underlying = mock[Underlying]
+      underlying.isTraceEnabled returns true
+      val logger = Logger(underlying)
+      logger.trace(Message)
+      there was one(underlying).trace(Message)
+      logger.trace(Message, "p1")
+      there was one(underlying).trace(Message, "p1")
+      logger.trace(Message, "p1", "p2")
+      there was one(underlying).trace(Message, "p1", "p2")
+      logger.trace(Message, Array("p1", "p2", "p3"))
+      there was one(underlying).trace(Message, Array("p1", "p2", "p3"))
     }
   }
 }
--- scalalogging-slf4j/src/main/scala/com/typesafe/scalalogging/Logger.scala
+++ scalalogging-slf4j/src/main/scala/com/typesafe/scalalogging/Logger.scala
@@ -32,28 +32,59 @@ object Logger {
 /**
  * Convenient and performant wrapper around the given underlying ''org.slf4j.Logger''.
  *
- * Convenient, because it is not necessary to write the check-enabled idiom (check whether the a particular log level is enabled) manually.
+ * Convenient, because you can use string formatting, string interpolation or whatever you want
+ * without thinking too much about performance.
  * Performant, because by using macros the log methods are expanded inline to the check-enabled idiom.
  */
 final class Logger private (val underlying: Underlying) {
 
   def error(message: String): Unit = macro LoggerMacros.error
 
+  def error(message: String, param: AnyRef): Unit = macro LoggerMacros.errorP
+
+  def error(message: String, param1: AnyRef, param2: AnyRef): Unit = macro LoggerMacros.errorP2
+
+  def error(message: String, params: Array[AnyRef]): Unit = macro LoggerMacros.errorPs
+
   def error(message: String, t: Throwable): Unit = macro LoggerMacros.errorT
 
   def warn(message: String): Unit = macro LoggerMacros.warn
+
+  def warn(message: String, param: AnyRef): Unit = macro LoggerMacros.warnP
+
+  def warn(message: String, param1: AnyRef, param2: AnyRef): Unit = macro LoggerMacros.warnP2
+
+  def warn(message: String, params: Array[AnyRef]): Unit = macro LoggerMacros.warnPs
 
   def warn(message: String, t: Throwable): Unit = macro LoggerMacros.warnT
 
   def info(message: String): Unit = macro LoggerMacros.info
 
+  def info(message: String, param: AnyRef): Unit = macro LoggerMacros.infoP
+
+  def info(message: String, param1: AnyRef, param2: AnyRef): Unit = macro LoggerMacros.infoP2
+
+  def info(message: String, params: Array[AnyRef]): Unit = macro LoggerMacros.infoPs
+
   def info(message: String, t: Throwable): Unit = macro LoggerMacros.infoT
 
   def debug(message: String): Unit = macro LoggerMacros.debug
 
+  def debug(message: String, param: AnyRef): Unit = macro LoggerMacros.debugP
+
+  def debug(message: String, param1: AnyRef, param2: AnyRef): Unit = macro LoggerMacros.debugP2
+
+  def debug(message: String, params: Array[AnyRef]): Unit = macro LoggerMacros.debugPs
+
   def debug(message: String, t: Throwable): Unit = macro LoggerMacros.debugT
 
   def trace(message: String): Unit = macro LoggerMacros.trace
+
+  def trace(message: String, param: AnyRef): Unit = macro LoggerMacros.traceP
+
+  def trace(message: String, param1: AnyRef, param2: AnyRef): Unit = macro LoggerMacros.traceP2
+
+  def trace(message: String, params: Array[AnyRef]): Unit = macro LoggerMacros.tracePs
 
   def trace(message: String, t: Throwable): Unit = macro LoggerMacros.traceT
 }
@@ -66,12 +97,42 @@ private object LoggerMacros {
     if (c.prefix.splice.underlying.isErrorEnabled) c.prefix.splice.underlying.error(message.splice)
   )
 
+  def errorP(c: LoggerContext)(message: c.Expr[String], param: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isErrorEnabled)
+      c.prefix.splice.underlying.error(message.splice, param.splice)
+  )
+
+  def errorP2(c: LoggerContext)(message: c.Expr[String], param1: c.Expr[AnyRef], param2: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isErrorEnabled)
+      c.prefix.splice.underlying.error(message.splice, param1.splice, param2.splice)
+  )
+
+  def errorPs(c: LoggerContext)(message: c.Expr[String], params: c.Expr[Array[AnyRef]]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isErrorEnabled)
+      c.prefix.splice.underlying.error(message.splice, params.splice)
+  )
+
   def errorT(c: LoggerContext)(message: c.Expr[String], t: c.Expr[Throwable]) = c.universe.reify(
     if (c.prefix.splice.underlying.isErrorEnabled) c.prefix.splice.underlying.error(message.splice, t)
   )
 
   def warn(c: LoggerContext)(message: c.Expr[String]) = c.universe.reify(
     if (c.prefix.splice.underlying.isWarnEnabled) c.prefix.splice.underlying.warn(message.splice)
+  )
+
+  def warnP(c: LoggerContext)(message: c.Expr[String], param: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isWarnEnabled)
+      c.prefix.splice.underlying.warn(message.splice, param.splice)
+  )
+
+  def warnP2(c: LoggerContext)(message: c.Expr[String], param1: c.Expr[AnyRef], param2: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isWarnEnabled)
+      c.prefix.splice.underlying.warn(message.splice, param1.splice, param2.splice)
+  )
+
+  def warnPs(c: LoggerContext)(message: c.Expr[String], params: c.Expr[Array[AnyRef]]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isWarnEnabled)
+      c.prefix.splice.underlying.warn(message.splice, params.splice)
   )
 
   def warnT(c: LoggerContext)(message: c.Expr[String], t: c.Expr[Throwable]) = c.universe.reify(
@@ -82,6 +143,21 @@ private object LoggerMacros {
     if (c.prefix.splice.underlying.isInfoEnabled) c.prefix.splice.underlying.info(message.splice)
   )
 
+  def infoP(c: LoggerContext)(message: c.Expr[String], param: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isInfoEnabled)
+      c.prefix.splice.underlying.info(message.splice, param.splice)
+  )
+
+  def infoP2(c: LoggerContext)(message: c.Expr[String], param1: c.Expr[AnyRef], param2: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isInfoEnabled)
+      c.prefix.splice.underlying.info(message.splice, param1.splice, param2.splice)
+  )
+
+  def infoPs(c: LoggerContext)(message: c.Expr[String], params: c.Expr[Array[AnyRef]]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isInfoEnabled)
+      c.prefix.splice.underlying.info(message.splice, params.splice)
+  )
+
   def infoT(c: LoggerContext)(message: c.Expr[String], t: c.Expr[Throwable]) = c.universe.reify(
     if (c.prefix.splice.underlying.isInfoEnabled) c.prefix.splice.underlying.info(message.splice, t)
   )
@@ -90,12 +166,42 @@ private object LoggerMacros {
     if (c.prefix.splice.underlying.isDebugEnabled) c.prefix.splice.underlying.debug(message.splice)
   )
 
+  def debugP(c: LoggerContext)(message: c.Expr[String], param: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isDebugEnabled)
+      c.prefix.splice.underlying.debug(message.splice, param.splice)
+  )
+
+  def debugP2(c: LoggerContext)(message: c.Expr[String], param1: c.Expr[AnyRef], param2: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isDebugEnabled)
+      c.prefix.splice.underlying.debug(message.splice, param1.splice, param2.splice)
+  )
+
+  def debugPs(c: LoggerContext)(message: c.Expr[String], params: c.Expr[Array[AnyRef]]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isDebugEnabled)
+      c.prefix.splice.underlying.debug(message.splice, params.splice)
+  )
+
   def debugT(c: LoggerContext)(message: c.Expr[String], t: c.Expr[Throwable]) = c.universe.reify(
     if (c.prefix.splice.underlying.isDebugEnabled) c.prefix.splice.underlying.debug(message.splice, t)
   )
 
   def trace(c: LoggerContext)(message: c.Expr[String]) = c.universe.reify(
     if (c.prefix.splice.underlying.isTraceEnabled) c.prefix.splice.underlying.trace(message.splice)
+  )
+
+  def traceP(c: LoggerContext)(message: c.Expr[String], param: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isTraceEnabled)
+      c.prefix.splice.underlying.trace(message.splice, param.splice)
+  )
+
+  def traceP2(c: LoggerContext)(message: c.Expr[String], param1: c.Expr[AnyRef], param2: c.Expr[AnyRef]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isTraceEnabled)
+      c.prefix.splice.underlying.trace(message.splice, param1.splice, param2.splice)
+  )
+
+  def tracePs(c: LoggerContext)(message: c.Expr[String], params: c.Expr[Array[AnyRef]]) = c.universe.reify(
+    if (c.prefix.splice.underlying.isTraceEnabled)
+      c.prefix.splice.underlying.trace(message.splice, params.splice)
   )
 
   def traceT(c: LoggerContext)(message: c.Expr[String], t: c.Expr[Throwable]) = c.universe.reify(
